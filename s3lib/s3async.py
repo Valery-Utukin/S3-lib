fb7028c046ca618cfd07cb69c0f971af60df0d9e
Valery-Utukin <valeryvumvum@gmail.com>
2025-03-27 16:39:11 +0700

    Get all files ready

---
+++ s3lib/s3async.py
@@ -0,0 +1,5 @@
+
+
+class S3Async:
+    def __init__(self):
+        print("S3Async just created!")
---
+++ s3lib/s3sync.py
@@ -0,0 +1,5 @@
+
+
+class S3Sync:
+    def __init__(self):
+        print("S3Sync just created!")
